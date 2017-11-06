0bc2018a7a84def6b2e4d481ad940a8b3f57270e
yichao.wang <wangycbeyond@gmail.com>
2017-11-06 18:16:37 +0800

    update inception

---
+++ inception/inception_v3.py
@@ -0,0 +1,65 @@
+from keras.applications.inception_v3 import InceptionV3
+from keras.preprocessing import image
+from keras.models import Model
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras import backend as K
+from keras.preprocessing import image
+
+def get_batches(path, gen=image.ImageDataGenerator(), shuffle=True, batch_size=8, class_mode='categorical'):
+  return gen.flow_from_directory(path, target_size=(224, 224),
+                                 class_mode=class_mode, shuffle=shuffle, batch_size=batch_size)
+
+train_batch = get_batches('keras_image/train', batch_size=100)
+eval_batch = get_batches('keras_image/eval', shuffle=False, batch_size=100)
+
+# create the base pre-trained model
+base_model = InceptionV3(weights='imagenet', include_top=False)
+
+# add a global spatial average pooling layer
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+# let's add a fully-connected layer
+x = Dense(1024, activation='relu')(x)
+# and a logistic layer -- let's say we have 200 classes
+predictions = Dense(132, activation='softmax')(x)
+
+# this is the model we will train
+model = Model(inputs=base_model.input, outputs=predictions)
+
+# first: train only the top layers (which were randomly initialized)
+# i.e. freeze all convolutional InceptionV3 layers
+for layer in base_model.layers:
+    layer.trainable = False
+
+# compile the model (should be done *after* setting layers to non-trainable)
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+
+# train the model on the new data for a few epochs
+model.fit_generator(train_batch, int(train_batch.samples/train_batch.batch_size), 20, validation_data=eval_batch, validation_steps=int(eval_batch.samples/eval_batch.batch_size))
+
+# at this point, the top layers are well trained and we can start fine-tuning
+# convolutional layers from inception V3. We will freeze the bottom N layers
+# and train the remaining top layers.
+
+# let's visualize layer names and layer indices to see how many layers
+# we should freeze:
+for i, layer in enumerate(base_model.layers):
+   print(i, layer.name)
+
+# we chose to train the top 2 inception blocks, i.e. we will freeze
+# the first 249 layers and unfreeze the rest:
+for layer in model.layers[:249]:
+   layer.trainable = False
+for layer in model.layers[249:]:
+   layer.trainable = True
+
+# we need to recompile the model for these modifications to take effect
+# we use SGD with a low learning rate
+from keras.optimizers import SGD
+model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy')
+
+# we train our model again (this time fine-tuning the top 2 inception blocks
+# alongside the top Dense layers
+model.fit_generator(train_batch, int(train_batch.samples/train_batch.batch_size), 10, validation_data=eval_batch, validation_steps=int(eval_batch.samples/eval_batch.batch_size))
+
+model.save('model.h5')
