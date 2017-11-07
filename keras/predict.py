c963c925e0706bce94b8d841d335f66bfae31d1a
yichao.wang <wangycbeyond@gmail.com>
2017-11-07 20:39:09 +0800

    update predict

--- keras/predict.py
+++ keras/predict.py
@@ -6,8 +6,8 @@ import pandas as pd
 import argparse
 from keras.preprocessing import image
 
-def get_batches(path, gen=image.ImageDataGenerator(), shuffle=True, batch_size=8, class_mode='categorical'):
-    return gen.flow_from_directory(path, target_size=(224, 224),
+def get_batches(path, gen=image.ImageDataGenerator(), shuffle=False, batch_size=8, class_mode='categorical'):
+    return gen.flow_from_directory(path, target_size=(299, 299),
                                    class_mode=class_mode, shuffle=shuffle, batch_size=batch_size)
 
 
@@ -18,16 +18,32 @@ def saveImageList(images, labels, save_to_file):
     data = pd.DataFrame(dic)
     data.to_csv(save_to_file, index=None, header=['image_name', 'category'])
 
-def predict(output_dir, test_dir):
-    model_path = output_dir + '/model.h5'
+def predict(output_dir, test_dir, model_path):
+    model_path = model_path
     res_path = output_dir + '/submit.csv'
 
     model = load_model(model_path)
+    # X = None
+    # for i in range(8):
+    #     img = image.load_img("transferred_test/test/" +str(i)+ ".jpg", target_size=(299, 299))
+    #     x = image.img_to_array(img)
+    #     x = np.expand_dims(x, axis=0)
+    #     if X is None:
+    #         X =x
+    #     else:
+    #         X = np.concatenate([X, x])
+    # preds = model.predict(X)
 
-    test_batches = get_batches(test_dir)
+    datagen = image.ImageDataGenerator(
+        rescale=1. / 255,
+    )
+    test_batches = get_batches(test_dir, datagen)
+    preds = model.predict_generator(test_batches, 1)
 
-    labels = model.predict_generator(test_batches, 1)
-    images = np.list([(str(i) + ".jpg") for i in range(len(labels))])
+    labels = [np.argmax(np.array(l)) for l in preds]
+    print(len(preds[0]), labels)
+    images = np.array([(str(i) + ".jpg") for i in range(len(labels))])
+    print(images)
     saveImageList(images, labels, res_path)
 
 
@@ -42,7 +58,10 @@ if __name__ == '__main__':
       '--test-dir',
       required=True
     )
-
+    parser.add_argument(
+      '--model-path',
+      required=True
+    )
     args = parser.parse_args()
     arguments = args.__dict__
 
